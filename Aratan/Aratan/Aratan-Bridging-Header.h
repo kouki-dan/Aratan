d51cb880012366f83e90c12aa4eab68ce06975dd
Kouki Saito <dan.addr.skd@gmail.com>
2015-01-18 20:57:02 +0900

    Add basic nin nin

--- Aratan/Aratan/Aratan-Bridging-Header.h
+++ Aratan/Aratan/Aratan-Bridging-Header.h
@@ -1,4 +1,6 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
--- Aratan/Aratan/ViewController.swift
+++ Aratan/Aratan/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        OEPocketsphinxController.sharedInstance().setActive(true, error: nil)
+        
+        let lmGenerator = OELanguageModelGenerator()
+        let words = ["WORD", "ALOUD", "SPEAK", "TANGO", "LEARNING"]
+        let name = "NameIWantForMyLanguageModelFiles";
+        
+        let err = lmGenerator.generateLanguageModelFromArray(words,
+            withFilesNamed: name,
+            forAcousticModelAtPath: OEAcousticModel.pathToModel("AcousticModelEnglish"))
+        
+
+        if err == nil{
+            let lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModelWithRequestedName("NameIWantForMyLanguageModelFiles")
+            let dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionaryWithRequestedName("NameIWantForMyLanguageModelFiles")
+        }
+        else{
+            print(err)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
