e5e834800c36159afe7a1718f2f5a71e038d4db6
Kouki Saito <dan.addr.skd@gmail.com>
2015-01-18 19:07:31 +0900

    Add OpenEars

---
+++ Aratan/Aratan/Aratan-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Aratan-Bridging-Header.h
+//  Aratan
+//
+//  Created by Kouki Saito on 2015/01/18.
+//  Copyright (c) 2015年 Team. Aratan. All rights reserved.
+//
+
+#import <Slt/Slt.h>
+
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEPocketsphinxController.h>
+
+
