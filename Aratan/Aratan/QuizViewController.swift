810c07c97a06023ea287a12a68f36cc9d12d0fa0
Kouki Saito <dan.addr.skd@gmail.com>
2015-01-19 21:34:17 +0900

    Implement basic ui for quiz view controller

---
+++ Aratan/Aratan/QuizViewController.swift
@@ -0,0 +1,14 @@
+//
+//  QuizViewController.swift
+//  Aratan
+//
+//  Created by Kouki Saito on 2015/01/19.
+//  Copyright (c) 2015年 Team. Aratan. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+}
+
--- Aratan/Aratan/WordRecognition.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WordRecognition.swift
-//  Aratan
-//
-//  Created by Kouki Saito on 2015/01/18.
-//  Copyright (c) 2015年 Team. Aratan. All rights reserved.
-//
-
-import Foundation
