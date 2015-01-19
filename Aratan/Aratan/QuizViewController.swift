f768f72cd77a84fdf749482af20bfd42a3a0e76d
Kouki Saito <dan.addr.skd@gmail.com>
2015-01-19 21:40:20 +0900

    Add labels

--- Aratan/Aratan/QuizViewController.swift
+++ Aratan/Aratan/QuizViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let wordsArray = [
+            ["WORD", "ALOUD", "SPEAK", "TANGO", "LEARNING", "NEXT"],
+            ["TEST", "PLAY", "FIX", "SUPER", "GOING", "NEXT"],
+            ["HAVE", "RICH", "GOOD", "Python", "MAIN", "NEXT"],
+            ["PRODUCT", "SCIENCE", "HEAD", "MAINTAIN", "NOW", "NEXT"],
+            ["TRAIN", "CREED", "STATUE", "LIBERTY", "LITTLE", "NEXT"],
+            ["REALIZE", "OBJECT", "ART", "ABSTRACT", "RICHIE", "NEXT"],
+            ["IMPULSE", "SOMETHING", "SALE", "LUXURIES", "PURCHASE", "NEXT"],
+            ["ANTICIPATE", "EMPLOY", "CUT", "COMPETENT", "MECHANIC", "NEXT"],
+            ["BIRTHRATE", "STEADILY", "INHABITANT", "MUCH", "RECOMMEND", ],
+        ]
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
