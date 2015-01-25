14b9e60e26f953ca023e4850522b72963aa8257c
Ryota Kabutoya <ryu15833@gmail.com>
2015-01-25 18:20:54 +0900

    画面を作った

---
+++ Aratan/Aratan/WordListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WordListViewCell.swift
+//  Aratan
+//
+//  Created by kabutoya ryota on 2015/01/25.
+//  Copyright (c) 2015年 Team. Aratan. All rights reserved.
+//
+
+import UIKit
+
+class WordListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
