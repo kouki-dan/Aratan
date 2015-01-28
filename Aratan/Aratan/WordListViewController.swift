520f69cb70c3e33e1ed354883c9cdee0c402a5ad
Ryota Kabutoya <ryu15833@gmail.com>
2015-01-28 17:07:45 +0900

    Implement changed level for wordListView

--- Aratan/Aratan/WordListViewController.swift
+++ Aratan/Aratan/WordListViewController.swift
@@ -12,8 +12,8 @@ import CoreData
 class WordListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    var texts = ["hello", "world", "hello", "Swift"]
     var wordsArray:[Word] = []
+    var level = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,11 +47,13 @@ class WordListViewController: UIViewController, UITableViewDataSource, UITableVi
         let appDelegate: AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
         let context: NSManagedObjectContext = appDelegate.managedObjectContext!
         
+        wordsArray = []
         
         let wordEntity: NSEntityDescription! = NSEntityDescription.entityForName("Word", inManagedObjectContext: context)
         
         let request = NSFetchRequest()
         request.entity = wordEntity
+        request.predicate = NSPredicate(format: "level = %d", self.level)
         var error:NSErrorPointer! = NSErrorPointer()
         
         if let results = context.executeFetchRequest(request, error: nil) {
@@ -64,6 +66,46 @@ class WordListViewController: UIViewController, UITableViewDataSource, UITableVi
         
     }
     
+    func setLevel(level:Int){
+        self.level = level
+    }
+    
+    func levelSelect(level:Int){
+        setLevel(level)
+    }
+    
+    @IBAction func levelChange(sender: AnyObject) {
+        let actionSheet:UIAlertController = UIAlertController(title:"Levelを選んでください！",
+            message: "Please select level you want to play!",
+            preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        for var i = 1; i <= 5; i++ {
+            {() -> Void in
+                var level = i
+                let levelAction:UIAlertAction = UIAlertAction(title: "Level \(i)",
+                    style: UIAlertActionStyle.Default,
+                    handler:{
+                        (action:UIAlertAction!) -> Void in
+                        self.levelSelect(level)
+                        self.loadWordData()
+                        self.tableView.reloadData()
+                })
+                
+                actionSheet.addAction(levelAction)
+                }()
+        }
+        
+        let cancelAction:UIAlertAction = UIAlertAction(title: "Cancel",
+            style: UIAlertActionStyle.Cancel,
+            handler:{
+                (action:UIAlertAction!) -> Void in
+                println("Cancel")
+        })
+        actionSheet.addAction(cancelAction)
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+
+    }
 
     /*
     // MARK: - Navigation
